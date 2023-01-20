d84068b707d762c9bac5795dc4a85540cefe4b38
dinarab2310 <filama92@mail.ru>
2023-01-20 15:51:53 +0600

    test ADMC-246

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import sys
+# import sys
+#
+#
+# def sum_two(a, b):
+#     return a + b
+#
+#
+# def main():
+#     args = sys.argv[1:]
+#     a = int(args[0])
+#     b = int(args[1])
+#     print(sum_two(a, b))
+#
+#
+# main()
 
 
-def sum_two(a, b):
-    return a + b
-
-
-def main():
-    args = sys.argv[1:]
-    a = int(args[0])
-    b = int(args[1])
-    print(sum_two(a, b))
-
-
-main()
